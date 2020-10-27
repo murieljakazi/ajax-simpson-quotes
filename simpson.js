d3bbd1e57ad9aabaa71385067b968542bba27dab
Muriel Jakazi <murielnjakazi@gmail.com>
2020-10-27 09:50:41 +0100

    axios get to consume API

---
+++ simpson.js
@@ -0,0 +1,21 @@
+function fetchPokemonJSON() {
+    // Feel free to download this HTML and edit it, to use another Pokemon ID
+    const url = `https://simpsons-quotes-api.herokuapp.com/quotes`;
+    axios.get(url)
+      .then(function(response) {
+        return response.data; // response.data instead of response.json() with fetch
+      })
+      .then(function(simpson) {
+        console.log('data decoded from JSON:', simpson);
+  
+        // Build a block of HTML
+        const simpsonHtml = `
+          <p><strong>${simpson[0].character}</strong></p>
+          <img src="${simpson[0].image}" />
+          <p><strong>${simpson[0].quote}<strong/></p>
+        `;
+        document.querySelector('#simpson').innerHTML = simpsonHtml;
+      });
+  }
+  
+  fetchPokemonJSON();
